dff6f2fa8d7f3f49383a7dd038bcdc0d7b1ae065
Rokibul Islam <rokibulislam@Rokibuls-MacBook-Pro.local>
2025-10-15 14:43:08 +0600

    implement footer component

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 "use client"
 import Header from "@/components/Header";
 import BlogList from "@/components/BlogList";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <div>
       <Header />
       <BlogList />
+      <Footer />
     </div>
   );
 }
