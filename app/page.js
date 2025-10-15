4b19af07383815bdbf3057c1d2d080861320a4e4
Rokibul Islam <rokibulislam@Rokibuls-MacBook-Pro.local>
2025-10-15 13:19:01 +0600

    implement blogItem component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
+"use client"
 import Header from "@/components/Header";
+import BlogItem from "@/components/BlogItem";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <BlogItem />
     </div>
   );
 }
